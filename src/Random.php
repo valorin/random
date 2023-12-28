970ccd4efd8b690148ebba33f30cfe6160db44f4
Stephen Rees-Carter <stephen@rees-carter.net>
2023-12-28 11:48:08 +1000

    Adding fallback

--- src/Random.php
+++ src/Random.php
@@ -6,18 +6,27 @@ use Random\Randomizer;
 
 class Random
 {
-    protected static Randomizer $randomizer;
+    protected static ?Randomizer $randomizer;
 
     /**
      * Generate a random number between $min and $max.
      */
     public static function number(int $min, int $max): int
     {
-        return self::randomizer()->getInt($min, $max);
+        return self::randomizer(
+            function (Randomizer $randomizer) use ($min, $max) {
+                return $randomizer->getInt($min, $max);
+            },
+            function () use ($min, $max) {
+                return random_int($min, $max);
+            }
+        );
     }
 
-    protected static function randomizer(): Randomizer
+    protected static function randomizer(callable $exists, callable $fallback)
     {
-        return self::$randomizer ??= new Randomizer;
+        return class_exists(Randomizer::class)
+            ? $exists(self::$randomizer ??= new Randomizer)
+            : $fallback();
     }
 }
